2aa7b0b968548ef7c59ab68c94b61f65179607fd
Miguel Barahona <miguel.barahona.12@hotmail.com>
2018-08-27 10:13:18 -0300

    Login Nuevo
    Se agrega css y librerias para nuevo estilo de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,61 +1,72 @@
 @extends('front.layout')
 
 @section('main')
-<div class="page type-page status-publish has-post-thumbnail hentry">
-    <section class="page-title-small border-bottom-mid-gray border-top-mid-gray blog-single-page-background bg-gray">
-        <div class="container-fluid">
-            <div class="col-md-12 col-sm-12 col-xs-12 text-center"><span class="text-extra-small text-uppercase alt-font right-separator blog-single-page-meta">ingresar como usuario</span>
-                <h2 class="title-small position-reletive font-weight-700 text-uppercase text-mid-gray blog-headline right-separator entry-title blog-single-page-title no-margin-bottom">Login</h2>
-            </div>
-        </div>
-    </section>
-    <section class="margin-three sm-margin-eight-top xs-margin-twelve-top no-margin-lr">
-        <div class="container">
-            <div class="row">
-                <div class="entry-content">
-                    @if (session('confirmation-success'))
+<div class="limiter">
+		<div class="container-login100" style="background-image: url('images/FB_IMG_1535034758934.jpg');">
+			<div class="wrap-login100 p-b-30">
+						@if (session('confirmation-success'))
                         @component('front.components.alert')
                             @slot('type')
                                 success
                             @endslot
                             {!! session('confirmation-success') !!}
                         @endcomponent
-                    @endif
-                    @if (session('confirmation-danger'))
-                        @component('front.components.alert')
-                            @slot('type')
-                                error
-                            @endslot
-                            {!! session('confirmation-danger') !!}
-                        @endcomponent
-                    @endif
-                    <h3>@lang('Login')</h3>
-                    <form role="form" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
+                        @endif
+                        @if (session('confirmation-danger'))
+                            @component('front.components.alert')
+                                @slot('type')
+                                    error
+                                @endslot
+                                {!! session('confirmation-danger') !!}
+                            @endcomponent
+                        @endif
+				<form class="login100-form validate-form" role="form" method="POST" action="{{ route('login') }}">
+                    <div class="login100-form-avatar">
+						<img src="images/logos/unionCristiana@2x-1.jpg" alt="AVATAR">
+                    </div>
+                    </br>
+                    {{ csrf_field() }}
                         @if ($errors->has('log'))
                             @component('front.components.error')
                                 {{ $errors->first('log') }}
                             @endcomponent
                         @endif   
-                        <input id="log" type="text" placeholder="@lang('Login')" class="input-field medium-input" name="log" value="{{ old('log') }}" required autofocus>
-                        <input id="password" type="password" placeholder="@lang('Password')" class="input-field medium-input"  name="password" required>
-                        <label class="add-bottom">
-                            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> 
-                            <span class="label-text">@lang('Remember me')</span>
-                        </label>
-                        <input class="btn btn-border btn-small text-uppercase font-weight-700 letter-spacing-1 alt-font" type="submit" value="@lang('Login')">
-                        <label class="add-bottom">
-                            <a href="{{ route('password.request') }}">
+					<div class="wrap-input100 validate-input m-b-10" data-validate = "Username is required">
+                        <input id="log" type="text" placeholder="@lang('Login')" class="input100" name="log" value="{{ old('log') }}" required autofocus>
+						<span class="focus-input100"></span>
+						<span class="symbol-input100" >
+							<i class="fa fa-user" style="margin-bottom: 5%;" ></i>
+						</span>
+					</div>
+
+					<div class="wrap-input100 validate-input m-b-10" data-validate = "Password is required">
+                        <input id="password" type="password" placeholder="@lang('Password')" class="input100"  name="password" required>
+						<span class="focus-input100"></span>
+						<span class="symbol-input100" >
+							<i class="fa fa-lock" style="margin-bottom: 5%;"></i>
+						</span>
+					</div>
+
+					<div class="container-login100-form-btn p-t-10">
+						<button class="login100-form-btn" type="submit" value="@lang('Login')">
+							Login
+						</button>
+					</div>
+
+					<div class="text-center w-full p-t-25">
+                         <a href="{{ route('password.request') }}" class="txt1">
                                 @lang('Forgot Your Password?')
-                            </a><br>
-                            <a href="{{ route('register') }}">
+                        </a>
+					</div>
+
+					<div class="text-center w-full">
+                        <a href="{{ route('register') }}" class="txt1">
                                 @lang('Not registered?')
-                            </a>
-                        </label>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </section>
-</div>
+                                <i class="fa fa-long-arrow-right"></i>
+                        </a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
 @endsection
