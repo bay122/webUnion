d6cc06bf09338bb36b232ec0d532ea17e90b561f
Desarrollo <desarrollo.web.unioncristiana@gmail.com>
2018-10-23 10:44:36 -0300

    rutas basicas para sitio

--- routes/web.php
+++ routes/web.php
@@ -32,23 +32,6 @@ Route::prefix('informacion')->namespace('Front')->group(function () {
     Route::name('ministerios')->get('/ministerios', 'UCController@ministerios', ['as' => 'site.ministerios']);
 });
 
-// Posts and comments
-Route::prefix('posts')->namespace('Front')->group(function () {
-    Route::name('posts.display')->get('{slug}', 'PostController@show');
-    Route::name('posts.tag')->get('tag/{tag}', 'PostController@tag');
-    Route::name('posts.search')->get('', 'PostController@search');
-    Route::name('posts.comments.store')->post('{post}/comments', 'CommentController@store');
-    Route::name('posts.comments.comments.store')->post('{post}/comments/{comment}/comments', 'CommentController@store');
-    Route::name('posts.comments')->get('{post}/comments/{page}', 'CommentController@comments');
-});
-
-Route::resource('comments', 'Front\CommentController', [
-    'only' => ['update', 'destroy'],
-    'names' => ['destroy' => 'front.comments.destroy']
-]);
-
-Route::name('category')->get('category/{category}', 'Front\PostController@category');
-
 // Authentification - login / registro
 Auth::routes();
 
@@ -64,7 +47,8 @@ Route::prefix('admin')->namespace('Back')->group(function () {
     Route::middleware('redac')->group(function () {
 
         Route::name('admin')->get('/', 'AdminController@index');
-
+        
+        /*
         // Posts
         Route::name('posts.seen')->put('posts/seen/{post}', 'PostController@updateSeen')->middleware('can:manage,post');
         Route::name('posts.active')->put('posts/active/{post}/{status?}', 'PostController@updateActive')->middleware('can:manage,post');
@@ -76,6 +60,12 @@ Route::prefix('admin')->namespace('Back')->group(function () {
 
         // Medias
         Route::view('medias', 'back.medias')->name('medias.index');
+        */
+       
+        // Video Home       
+         Route::resource('videos', 'ConfiguracionController', ['only' => [
+            'index','update'
+        ]]);
 
     });
 
@@ -118,18 +108,6 @@ Route::prefix('admin')->namespace('Back')->group(function () {
 
         Route::name('admin')->get('/', 'AdminController@index');
 
-        // Posts
-        Route::name('posts.seen')->put('posts/seen/{post}', 'PostController@updateSeen')->middleware('can:manage,post');
-        Route::name('posts.active')->put('posts/active/{post}/{status?}', 'PostController@updateActive')->middleware('can:manage,post');
-        Route::resource('posts', 'PostController');
-
-        // Notifications
-        Route::name('notifications.index')->get('notifications/{user}', 'NotificationController@index');
-        Route::name('notifications.update')->put('notifications/{notification}', 'NotificationController@update');
-
-        // Medias
-        Route::view('medias', 'back.medias')->name('medias.index');
-
         // Video Home       
          Route::resource('videos', 'ConfiguracionController', ['only' => [
             'index','update'
@@ -138,19 +116,3 @@ Route::prefix('admin')->namespace('Back')->group(function () {
     });
 
 });
-
-/*
-Route::group(['prefix' => 'comunion'], function(){
-    Route::get('/', 'Front\PagesTestController@home');
-    Route::get('/misiones', 'Front\PagesTestController@misiones', ['as' => 'site.misiones.articles']);
-    Route::get('/contact', 'Front\PagesTestController@contact', ['as' => 'site.contact']);
-    Route::get('/editorial', 'Front\PagesTestController@editorial', ['as' => 'site.editorial.articles']);
-    Route::get('/notas', 'Front\PagesTestController@notas', ['as' => 'site.notas.articles']);
-    Route::get('/ministerios', 'Front\PagesTestController@ministerios', ['as' => 'site.ministerios.articles']);
-    Route::get('/misiones', 'Front\PagesTestController@misiones', ['as' => 'site.misiones.articles']);
-    Route::get('/agenda', 'Front\PagesTestController@agenda', ['as' => 'site.agenda']);
-    Route::get('/sociales', 'Front\PagesTestController@sociales', ['as' => 'site.sociales']);
-    Route::get('/galeria', 'Front\PagesTestController@galery', ['as' => 'site.sociales.galery']);
-    Route::get('/404', 'Front\PagesTestController@notFound', ['as' => 'admin']);
-});
-*/
