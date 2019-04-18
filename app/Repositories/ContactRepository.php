a9d6af47a4dfecfa69a422b44d5e97111c5087bf
Desarrollo <desarrollo.web.unioncristiana@gmail.com>
2019-04-18 09:39:34 -0400

    se agrega documentacion y relacion en repositorio de contactos

--- app/Repositories/ContactRepository.php
+++ app/Repositories/ContactRepository.php
@@ -4,6 +4,9 @@ namespace App\Repositories;
 
 use App\Models\Contact;
 
+/**
+ * Docs: https://stackoverflow.com/questions/30231862/laravel-eloquent-has-with-wherehas-what-do-they-mean
+ */
 class ContactRepository
 {
     /**
@@ -15,10 +18,24 @@ class ContactRepository
      */
     public function getAll($nbrPages, $parameters)
     {
-        return Contact::with ('ingoing')
+    	/**
+    	 * La query antigua hacia las siguientes acciones:
+    	 * select count(*) as aggregate from `contacts`
+		 * select * from `contacts` order by `created_at` desc limit 3 offset 0
+		 * select * from `ingoings` where `ingoings`.`ingoing_id` in ('8', '9', '10') and `ingoings`.`ingoing_type` = 'App\Models\Contact'
+    	 */
+        /*return Contact::with ('ingoing')
             ->latest()
             ->when ($parameters['new'], function ($query) {
                 $query->has ('ingoing');
-            })->paginate($nbrPages);
+            })->paginate($nbrPages);*/
+
+        $contacts =  Contact::where('bo_estado', '=', '1')
+        		->when($parameters['new'], function ($query) {
+	                $query->where('bo_leido', '=', '0');
+	            })->orderBy('created_at', 'desc')->paginate($nbrPages);
+        //file_put_contents('php://stderr', PHP_EOL . print_r($nbrPages, TRUE). PHP_EOL, FILE_APPEND);
+        //file_put_contents('php://stderr', PHP_EOL . print_r($parameters, TRUE). PHP_EOL, FILE_APPEND);
+	    return $contacts;
     }
 }
