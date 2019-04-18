eb02810e2063164c8e8f5e0ffa5256db119dc438
Desarrollo <desarrollo.web.unioncristiana@gmail.com>
2019-04-18 09:36:02 -0400

    mejoras y validaciones de seguridad en ingresar un nuevo mensaje del usuario

--- app/Http/Controllers/Front/ContactController.php
+++ app/Http/Controllers/Front/ContactController.php
@@ -1,11 +1,14 @@
 <?php
 
 namespace App\Http\Controllers\Front;
-
+use Carbon\Carbon;
 use App\ {
+    Services\Access,
+    Services\Security,
+    Services\Helper,
     Http\Controllers\Controller,
     Http\Requests\ContactRequest,
-    Models\ministerio,
+    Models\Ministerio,
     Models\Contact
     
 };
@@ -19,7 +22,7 @@ class ContactController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest');
+        //$this->middleware('guest');
     }
 
     /**
@@ -37,14 +40,58 @@ class ContactController extends Controller
 
     /**
      * Store a newly created contact in storage.
-     *
+     * Docs: https://laravel.com/docs/5.8/queries#where-exists-clauses
+     * 		 https://stackoverflow.com/questions/39902831/class-app-carbon-carbon-not-found-laravel-5/39903149
      * @param  ContactRequest $request
      * @return \Illuminate\Http\Response
      */
     public function store(ContactRequest $request)
     {
-        Contact::create ($request->all ());
+    	//Valido todos los datos para evitar inyecciones SQL
+    	$name = Security::validar($request->input("name"), 'string');
+		$email = Security::validar($request->input("email"), 'string');
+		$message = Security::validar($request->input("message"), 'string');
+		$recap = Security::validar($request->input("recap"), 'string');
+		$id_ministerio = Security::validar($request->input("id_ministerio"), 'numero');
 
-        return back ()->with ('ok', __('Your message has been recorded, we will respond as soon as possible.'));
+		//Cargo el resultado del captcha
+        $result_recaptcha = Security::validarReCaptcha($request->input("recap"), 'contact');
+
+        //Valido si pasó el captcha
+        if($result_recaptcha["status"] != "ERROR"){
+        	//Valido que el mail no se encuentre registrado como spam
+	        $spam = Contact::where('email', $email)->where("bo_spam", 1)->exists();
+	        if($spam){
+	        	//Si es spam, devuelvo mensaje de error
+	        	return back ()->with ('error', __("Lo sentimos, por motivos de seguridad, su solicitud fue rechazada."));
+	        }
+	        else{
+	        	//Verifico si ha alcanzado el limite de mensajes por hora
+	        	$recent_contacts = Contact::where('email', $email)->where('created_at', '>', Carbon::now()->subHours(1)->toDateTimeString())->get();
+	        	if($recent_contacts->count() > 2){
+	        		//Si supero el limite, devuelvo mensaje de error
+	        		return back ()->with ('error', __("Ha enviado demaciados mensajes, por favor, intentelo nuevamente más tarde."));		
+	        	}
+	        	else{
+	        		//Verifico si, a pesar de haber pasado el captcha, obtuvo una puntuación baja.
+	        		//Si su puntuación es baja (WARNING), se marca mensaje para revisión.
+	        		$bo_validar = ($result_recaptcha["status"] == "WARNING") ? true : false;
+		        	$datos_contacto = array(
+		        			'name' => $name,
+							'email' => $email,
+							'message' => $message,
+							'bo_validar' => $bo_validar,
+							'id_ministerio' => $id_ministerio,
+
+		        		);
+		        	Contact::create ($datos_contacto);
+		            return back ()->with ('ok', __('Your message has been recorded, we will respond as soon as possible.'));
+	        	}
+	        }
+        }
+        else{
+        	//Si no paso el captcha, devuelvo el error.
+            return back ()->with ('error', __($result_recaptcha["msg"]));
+        }
     }
 }
