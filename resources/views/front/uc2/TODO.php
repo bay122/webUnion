6c9507b4cf63ab30a5b1b170096949d50f929899
Bay122 <jimenez.pablom@gmail.com>
2020-08-17 18:58:08 -0400

    modal en vista de inicio

---
+++ resources/views/front/uc2/TODO.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @todo  habilitar botón para volver a la parte superior de la página luego de hacer scroll.
+ *        El captcha podría interferir en su uso
+ *
+ * @todo  Actualizar menú de la cabecera,para que aparezca un menú desplegable.
+ *        layout_NEW_MENU y header_NEW_MENU tienen un ejemplo pero no se comporta correctamente en mobile
+ *        quizá se debería usar como base el menú desplegable de ejemplo y a ese ponerle opciones fijar
+ *        en vez de hacerlo alrevez.
+ *
+ * @todo  El formulario de encuesta debería estar en un step-form (wizzard)
+ *
+ * @todo  En el home debe aparece un popup con un carrusel integrado que tenga las últimas noticias.
+ *
+ * @todo  en los ministeios al lado del titulo seia bacan tener los "logos" de ministerios
+ *
+ * @todo  validar cómo ve la gente la sección de contácto (al parecer no ven el pin)
+ *
+ * @todo  hacer una v2 del formulario de encuesta, sin usar api google maps
+ */
